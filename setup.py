e6fabe6e708deadd8137713593c5f7d65376f3ed
Tzutori <38956752+Tzutori@users.noreply.github.com>
2020-09-07 17:11:35 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="watermelon-feature-selection",
+    version="0.0.3",
+    author="Xiang Xie",
+    author_email="xiang.xie.china@gmail.com",
+    description="A python package for watermelon feature selection method",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=['pandas>=1.0.4','numpy>=1.18.5','scipy>=1.4.1','sklearn>=0.23.1'],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    python_requires='>=3.7',
+)
